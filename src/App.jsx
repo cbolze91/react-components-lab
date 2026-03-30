49cb1e64e3e90f6ce3fb4b594f0bd2050fe689cd
Christina <christinamarieelaine@gmail.com>
2026-03-30 16:21:39 -0700

    React Compenents WeathForecasts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import './App.css'
+import WeatherForecast from './Components/WeatherForecast/WeatherForecast.jsx';
+
+const weatherForecasts = [
+  {
+    day: 'Mon',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg',
+    imgAlt: 'sun icon',
+    conditions: 'sunny',
+    time: 'Morning',
+    id: 1,
+  },
+  {
+    day: 'Tue',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg',
+    imgAlt: 'moon icon',
+    conditions: 'clear',
+    time: 'Night',
+    id: 2,
+  },
+  {
+    day: 'Wed',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg',
+    imgAlt: 'clouds with lightning icon',
+    conditions: 'stormy',
+    time: 'All Day',
+    id: 3,
+  },
+  {
+    day: 'Thu',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg',
+    imgAlt: 'sun overcast by clouds icon',
+    conditions: 'overcast',
+    time: 'Evening',
+    id: 4,
+  },
+  {
+    day: 'Fri',
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg',
+    imgAlt: 'moon overcast by clouds icon',
+    conditions: 'cloudy',
+    time: 'Night',
+    id: 5,
+  },
+];
 
 const App = () => {
-
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Local Weather</h1>
+      <section>
+        {weatherForecasts.map((forecast) => (
+          <WeatherForecast
+            key = {forecast.id}
+            forecast = {forecast}
+         />
+         ))}    
+      </section>
+    </>
   );
 }
 
